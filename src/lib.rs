b0e98a03525f5dc01821913493424bb08702cc19
aburd <burdick.aaron@gmail.com>
2020-12-15 00:57:19 +0900

    add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,31 @@
+#![deny(missing_docs)]
+//! A simple key value store
+
 use std::collections::HashMap;
 
+/// The `KvStore` stores string key/value pairs.
+#[derive(Default)]
 pub struct KvStore {
     data: HashMap<String, String>,
 }
 
 impl KvStore {
+    /// A key-value store
     pub fn new() -> Self {
         Self {
             data: HashMap::new(),
         }
     }
 
+    /// Sets a value in the store
     pub fn set(&mut self, key: String, value: String) {
         self.data.insert(key, value);
     }
+    /// Gets a value from the store
     pub fn get(&self, key: String) -> Option<String> {
-        match self.data.get(&key) {
-            Some(value) => Some(value.to_owned()),
-            None => None,
-        }
+        self.data.get(&key).cloned()
     }
+    /// Removes a value from the store
     pub fn remove(&mut self, key: String) {
         self.data.remove(&key);
     }
