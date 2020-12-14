6475747f9924e0770c5d78e97560dacb1bef0d11
aburd <burdick.aaron@gmail.com>
2020-12-15 00:51:08 +0900

    implement lib

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+pub struct KvStore {
+    data: HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.data.insert(key, value);
+    }
+    pub fn get(&self, key: String) -> Option<String> {
+        match self.data.get(&key) {
+            Some(value) => Some(value.to_owned()),
+            None => None,
+        }
+    }
+    pub fn remove(&mut self, key: String) {
+        self.data.remove(&key);
+    }
+}
